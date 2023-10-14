97bc15b02311153665054806ea39a4585b39f263
kieutranquocson <kieutranquocson@gmail.com>
2023-10-14 16:51:54 +0700

    update getf() to use recursion , green4ever and regression test

--- src/main/java/com/giaolang/mathutil/core/MathUtility.java
+++ src/main/java/com/giaolang/mathutil/core/MathUtility.java
@@ -20,16 +20,29 @@ public class MathUtility {
     //n < 0 n >20 ko tính do 21! tràn kiểu long 
     //ko tính giai thừa âm !!!1
     //0! = 1! = 1
+//    public  static long getFactorial(int n) {
+//        long result =10 ;
+//        
+//        if (n < 0 || n > 20)
+//            throw new IllegalArgumentException( "Invalid");
+//        if (n == 0 || n == 1)
+//            return 1;
+//        for(int i = 2 ; i <= n; i++)
+//            result *= i;
+//        return result;
+//    }
+     //NÂNG CẤP HÀM TÍNH GIAI THỪA
+    //n! = 1.2.3...n
+    //5!= 1.2.3.4.5
     public  static long getFactorial(int n) {
-        long result =10 ;
+//        long result = 1 ;
         
         if (n < 0 || n > 20)
             throw new IllegalArgumentException( "Invalid");
         if (n == 0 || n == 1)
             return 1;
-        for(int i = 2 ; i <= n; i++)
-            result *= i;
-        return result;
+        
+        return n* getFactorial(n-1);// n! = n* (n-1)!
     }
-     
+    
 }
